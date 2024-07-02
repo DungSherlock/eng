aac419876d255b37856e1ed4cde0fc9793cd5d65
Huệ Dũng <dungsherlock@gmail.com>
2024-07-02 20:40:52 +0700

    quote

--- Speechify/all-voice.sh
+++ Speechify/all-voice.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-## bỏ "john" 
+# voice=("john" "presidential" "mrbeast" "snoop" "gwyneth" "nate" "jamie" "bwyneth" "cliff" "mary" "henry" "david" "emma" "kristy" "oliver" "benwilson" "tasha" "joe" "lisa" "george" "emily" "rob" "jessica" "aria" "kyle" "carly" "sally" "simon")
+
 voice=("presidential" "mrbeast" "snoop" "gwyneth" "nate" "jamie" "bwyneth" "cliff" "mary" "henry" "david" "emma" "kristy" "oliver" "benwilson" "tasha" "joe" "lisa" "george" "emily" "rob" "jessica" "aria" "kyle" "carly" "sally" "simon")
 
 for name in "${voice[@]}"; do
